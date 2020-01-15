65827627417ce9614c8183257ad79f4c7a6056c9
zhaoyunxing92 <2385585770@qq.com>
2020-01-15 17:17:01 +0800

    [add]ignore

---
+++ day02/operators/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// 运算
+func main() {
+	// * 算术运算
+	a := 20
+	b := 10
+
+	fmt.Println(a + b) //30
+	fmt.Println(a - b) //10
+	fmt.Println(a * b) // 200
+	fmt.Println(a / b) // 2
+	fmt.Println(a % b) // 2
+}
