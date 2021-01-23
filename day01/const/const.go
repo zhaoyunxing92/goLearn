16db13cdcfe6a2b92193ff536b8fe21384df2be8
zhaoyunxing <wb-zyx735784@alibaba-inc.com>
2021-01-23 10:50:10 +0800

    up:更新ioto使用

--- day01/const/const.go
+++ day01/const/const.go
@@ -57,6 +57,13 @@ const (
 	w, q                      //3 4
 )
 
+const (
+	p = 1 << iota //1
+	y             //2
+	x             //4
+	z = iota      //3
+)
+
 func main() {
 
 	fmt.Printf("a=%d, b=%d, c=%d \n", a, b, c) //a=10,b=10,c=10
@@ -70,4 +77,6 @@ func main() {
 	fmt.Printf("KB=%d, MB=%d, GB=%d, TB=%d \n", KB, MB, GB, TB) //1024 1048576 1073741824 1099511627776
 
 	fmt.Printf("e=%d, f=%d, g=%d, h=%d, w=%d, q=%d \n", e, f, g, h, w, q) //1 2 2 3 3 4
+
+	fmt.Printf("p=%d, y=%d, x=%d, z=%d \n", p, y, x, z)
 }
