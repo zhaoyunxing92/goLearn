2d62ce5993b01f5c0bf7e3c94867c67c04e87ee2
zhaoyunxing92 <2385585770@qq.com>
2021-05-30 17:03:43 +0800

    up:解析给默认值

--- yaml/domain/grade.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-type Grade struct {
-	Id    string  `yaml:"id"`
-	Name  string  `yaml:"name"`
-	Users []*User `yaml:"users"`
-}
--- yaml/domain/user.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-type User struct {
-	Name   string `yaml:"name" default:"sunny"`
-	Age    int    `yaml:"age" default:"20" json:"age,omitempty" property:"age"`
-	Height int    `yaml:"height" default:"190" json:"height,omitempty" property:"height"`
-}
